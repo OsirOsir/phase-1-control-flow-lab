cc7457e54d2ad51772aec8e6ec07804098022b8f
Philip osir Otieno <philiposir2@gmail.com>
2024-06-26 05:47:19 +0300

    added statements

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  } else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'That will be twenty bucks.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ?'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(note){
   // Write your code here!
+  switch(note) {
+    case 'generous':
+      return "Thank you so much." 
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    case 'thanks for everything':
+      return 'Bye.'
+  }
+
 }
